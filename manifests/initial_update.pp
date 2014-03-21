32a3871a60701df9ee6ed58ad2719d7e91aef640
Frederik Wagner <wagner@wagit.de>
2014-03-21 09:05:59 +0100

    corrected returnvalues of sa-update

--- manifests/initial_update.pp
+++ manifests/initial_update.pp
@@ -4,6 +4,7 @@ class spamassassin::initial_update {
   exec {'update spamassassin':
     command     => "su - ${spamassassin::user} -c 'sa-update --gpghomedir \$HOME/sa-update-keys'",
     refreshonly => true,
+    returns     => [0,1],
     path        => ['/bin','/usr/bin'],
   }
 
